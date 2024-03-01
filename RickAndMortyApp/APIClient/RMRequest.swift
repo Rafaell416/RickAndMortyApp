ec8584df9025ffbfaccff214fa6c5d2bbb23ae91
Rafael Villarreal <rvillarreal416@gmail.com>
2024-02-29 23:02:54 -0500

    feat: add custom api request object

--- RickAndMortyApp/APIClient/RMRequest.swift
+++ RickAndMortyApp/APIClient/RMRequest.swift
@@ -8,5 +8,54 @@
 import Foundation
 
 final class RMRequest {
+	private struct Constants {
+		static let baseURl = "https://rickandmortyapi.com/api"
+	}
 	
+	private let endpoint: RMEndpoint
+	
+	private let pathComponents: Set<String>?
+	
+	private let queryParameters: [URLQueryItem]
+	
+	private var urlString: String {
+		var string = Constants.baseURl
+		string += "/"
+		string += endpoint.rawValue
+		
+		if let pathComponents = pathComponents, !pathComponents.isEmpty {
+			string += "/"
+			pathComponents.forEach({
+				string += "/\($0)"
+			})
+		}
+		
+		if !queryParameters.isEmpty {
+			string += "?"
+			let argumentString = queryParameters.compactMap({
+				guard let value = $0.value else { return nil }
+				return "\($0.name)=\(value)"
+			}).joined(separator: "&")
+			
+			string += argumentString
+		}
+		
+		return string
+	}
+	
+	public let httpMethod = "GET"
+	
+	public var url: URL? {
+		return URL(string: urlString)
+	}
+	
+	init(
+		endpoint: RMEndpoint,
+		pathComponents: Set<String> = [],
+		queryParameters: [URLQueryItem] = []
+	) {
+		self.endpoint = endpoint
+		self.pathComponents = pathComponents
+		self.queryParameters = queryParameters
+	}
 }
